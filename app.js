6a23598f1da870361de5611e9c6fe30cb0daec8a
azrulwm <azrulazmi.wm@gmail.com>
2022-10-25 00:31:02 +0800

    successful using the mongoclient method to connect to db

--- app.js
+++ app.js
@@ -7,32 +7,30 @@ const app = express();
 // db connection
 let db;
 
-connectToDb(async (err) => {
+connectToDb((err) => {
   if (!err) {
     app.listen(3000, () => {
       console.log("app listening on port 3000");
-      db = getDb();
     });
 
-    console.log("wildcard");
-    console.log(db, "app.js");
+    db = getDb();
   }
 });
 
 //routes
 app.get("/books", (req, res) => {
-  // let books = [];
-  // const collection = db.collection("books");
+  let books = [];
+  const collection = db.collection("books");
   // console.log(collection);
-  // collection
-  //   .find()
-  //   .sort({ author: 1 })
-  //   .forEach((book) => books.push(book))
-  //   .then(() => {
-  //     res.status(200).json(books);
-  //   })
-  //   .catch(() => {
-  //     res.status(500).json({ error: "Could not fetch the documents" });
-  //   });
+  collection
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Could not fetch the documents" });
+    });
   // res.json({ mssg: "welcome to the API" });
 });
--- db.js
+++ db.js
@@ -5,33 +5,17 @@ let dbConnection;
 
 module.exports = {
   connectToDb: async (cb) => {
-    mongoose.connect("mongodb://localhost:27017/bookstore");
-
-    const connection = await mongoose.connection;
-    connection.on("error", console.error.bind(console, "connection error: "));
-    connection.once("open", async function () {
-      console.log("Connected successfully");
-
-      // dbConnection = connection.db;
-      // console.log(dbConnection);
-      // const collection = dbConnection.collection("books");
-      dbConnection = connection.db.collection("books");
-      console.log("for collection");
-      // console.log(collection);
-
-      // collection.find({}).toArray(function (err, data) {
-      //   console.log(data);
-      // });
-    });
-
-    return cb();
-    if (err) {
-      console.log(err);
-      return cb(err);
+    try {
+      const client = await MongoClient.connect("mongodb://localhost:27017");
+      dbConnection = client.db("bookstore");
+      console.log("database connected");
+      return cb();
+    } catch (error) {
+      console.log(error);
+      return cb(error);
     }
   },
-  getDb: async () => {
-    console.log(await dbConnection, "testing");
+  getDb: () => {
     return dbConnection;
   },
 };
